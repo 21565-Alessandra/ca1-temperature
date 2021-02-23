e07b6c89fa64ac4042bc84edc0d1c85474728830
reisa <alereis1002@gmail.com>
2021-02-23 22:12:15 +0000

    Commit CA1

--- src/com/oopdorsetcollege/TemperatureConverter.java
+++ src/com/oopdorsetcollege/TemperatureConverter.java
@@ -9,16 +9,16 @@ import java.awt.event.ActionListener;
 
 public class TemperatureConverter extends JFrame {
 
-    private JCheckBox checkCelsius;
-    private JCheckBox checkFahrenheit;
+    public JCheckBox checkCelsius;
+    public JCheckBox checkFahrenheit;
     public JTextField textInsert;
     public JTextField textOutput; //private JLabel labelOutput
-    private JButton btConversion;
-
-
+    public JButton btConversion;
 
     public TemperatureConverter() {
+
         super("Temperature Conversion");
+
 
         setLayout(new FlowLayout());
 
@@ -29,18 +29,18 @@ public class TemperatureConverter extends JFrame {
         add(checkFahrenheit);
 
         //add text field to input information
-        textInsert = new JTextField (20);
+        textInsert = new JTextField(20);
         textInsert.setFont(new Font("Serif", Font.PLAIN, 15));
         add(textInsert);
 
         //add text field to output information
-        textOutput = new JTextField (20);
+        textOutput = new JTextField(20);
         textOutput.setFont(new Font("Serif", Font.PLAIN, 15));
         add(textOutput);
 
         //button to convert
-        btConversion = new JButton ("Conversion");
-        add (btConversion);
+        btConversion = new JButton("Conversion");
+        add(btConversion);
 
 
         JPanel panel = new JPanel(new GridLayout(3, 2, 12, 6));
@@ -54,12 +54,42 @@ public class TemperatureConverter extends JFrame {
         buttonPanel.add(btConversion);
         add(buttonPanel, BorderLayout.SOUTH);
 
-        btConversion.addActionListener(new CelListener());
-        btConversion.addActionListener(new FahrListener());
+        //To organize the panel layout
+
+        btConversion.addActionListener(new ActionOne());
+        btConversion.addActionListener(new ActionTwo());
 
     } // end constructor
 
-    private class CelListener implements ActionListener {
+
+    private class ActionOne implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            if (checkCelsius.isSelected()) {
+                int celsiusToFahr = (int) ((9.0 / 5.0 * (((Double.parseDouble(textInsert.getText())) + 32))));
+                textInsert.setText(celsiusToFahr + " °F");
+                textOutput.requestFocus();
+                textOutput.selectAll();
+            }
+
+
+        }
+
+    }
+
+    private class ActionTwo implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+
+            if (checkFahrenheit.isSelected()) {
+                int fahrToCelsius = (int) ((5.0 / 9.0 * (((Double.parseDouble(textInsert.getText())) - 32))));
+                textInsert.setText(fahrToCelsius + " °C");
+                textOutput.requestFocus();
+                textOutput.selectAll();
+            }
+        }
+
+    /*private class CelListener implements ActionListener {
 
         public void actionPerformed(ActionEvent event) {
 
@@ -86,7 +116,8 @@ public class TemperatureConverter extends JFrame {
 
         }
     }
+*/
 
+    }
 
 }
-
